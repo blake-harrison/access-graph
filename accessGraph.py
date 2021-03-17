fe1f264088002bacea29d6ba3d8dbfa4b22f9f93
blake harrison <blakeharrison323@gmail.com>
2021-03-17 14:49:39 -0400

    added prepGraph function to initialize array

--- accessGraph.py
+++ accessGraph.py
@@ -14,6 +14,18 @@ import sys
 import os.path
 from os import path
 
+#note - numpy is used for array handling for the graph
+#if you do not have this package installed, run:
+#pip install numpy
+#from a terminal
+import numpy as np
+
+#defines infinity as a global constant
+#approximated at 9,999,999 because we assume no single 
+#   path length will come close to this
+#this can be adjusted to account for larger size values
+INF = 999999
+
 #reads in the input file
 #takes 5 parameters:
 #   inFile - the name of the input file
@@ -75,6 +87,42 @@ def printLists(G,R,I,E):
     print("E = ", E)
     print("\n")
 
+#loads all of the nodes into one list:
+#takes in 4 lists:
+#   G (the grocery stores) 
+#   R (residential areas) 
+#   I (intersections) (our artificial nodes for connectivity purposes)
+#   N (a list that will be filled all of the nodes)      
+def loadDijkstra(G,R,I,N):
+    for x in range(len(G)):
+        N.append(G[x][0])
+    for x in range(len(R)):
+        N.append(R[x][0])
+    for x in range(len(I)):
+        N.append(I[x][0])
+
+#turns the lists of nodes and edges into one graph
+#takes in 2 lists:
+#   N (all the nodes)
+#   E (all the edges)
+#
+#returns a two-dimensional list acting as the graph
+def prepGraph(N,E):
+    
+    #sets up an empty array of correct dimensions
+    #fills the array with the value nan as a placeholder
+    graph=np.empty((int(len(N)),int(len(N)))) * np.nan
+    
+    #loops through the edges, initializing the graph
+    for x in range(len(E)):
+        start = E[x][0]
+        stop = E[x][1]
+        dist = E[x][2]
+        graph[N.index(start)][N.index(stop)] = int(dist)
+    return graph
+        
+    
+
 def main():
     #user specifies the input file
     inFile = input("\n\n\n\nPlease Enter the Name of Your Datafile:\n")
@@ -89,22 +137,28 @@ def main():
     #reads the input file in
     readFile(inFile,G,R,I,E)
     
-    printLists(G,R,I,E)
+    #printLists(G,R,I,E)
     #print("\n\n\n")
     
     #splits each item in each list on spaces
     #i.e. ["R001 RA One"] becomes ["R001","RA","One"] 
-    for x in range(len(G)):
+    for x in range(0,len(G)):
         G[x] = G[x].split()
-    for x in range(len(R)):
+    for x in range(0,len(R)):
         R[x] = R[x].split()
-    for x in range(len(I)):
+    for x in range(0,len(I)):
         I[x] = I[x].split()
-    for x in range(len(E)):
+    for x in range(0,len(E)):
         E[x] = E[x].split()
     
-    printLists(G,R,I,E)
-        
+    #printLists(G,R,I,E)
+    
+    #adds a list that holds N, which contains all the nodes
+    N=[]
+    loadDijkstra(G,R,I,N)
+    graph=prepGraph(N,E) 
+    print(N)
+    print(graph)
 
 #executes main function
 if __name__ == "__main__":
